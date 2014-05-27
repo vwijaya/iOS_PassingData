b1a6e8465a0e67871893f4141427a27571757258
Valerino Wijaya <valerino@gmail.com>
2014-05-27 15:59:29 +0700

    Initial Commit

---
+++ Passing Data/VWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VWAppDelegate.h
+//  Passing Data
+//
+//  Created by Valerino on 5/27/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Passing DataTests/Passing_DataTests.m
@@ -0,0 +1,34 @@
+//
+//  Passing_DataTests.m
+//  Passing DataTests
+//
+//  Created by Valerino on 5/27/14.
+//  Copyright (c) 2014 VW. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Passing_DataTests : XCTestCase
+
+@end
+
+@implementation Passing_DataTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
